b87672f9070f194d21893be205e3472a3aa46177
Emily Lillie <emilylillie3@gmail.com>
2019-06-24 11:36:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,7 +37,7 @@ long_planeteer_calls(planeteer_calls)
 
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(potentially_cheesy_items)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
