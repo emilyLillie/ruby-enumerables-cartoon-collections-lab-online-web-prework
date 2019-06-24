6a639ff32a69eb5cfeae93aec28d141de4fc67bb
Emily Lillie <emilylillie3@gmail.com>
2019-06-24 11:34:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ end
 summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.map { |call| call.any? length > 4}
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
 long_planeteer_calls(planeteer_calls)
