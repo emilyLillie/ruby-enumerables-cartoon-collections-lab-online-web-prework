b748812a3fc4b3095775e87b038dfebfbdfdd9f9
Emily Lillie <emilylillie3@gmail.com>
2019-06-24 11:36:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -40,4 +40,5 @@ potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 def find_the_cheese(potentially_cheesy_items)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potentially_cheesy_items.find 
 end
